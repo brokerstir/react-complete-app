ee5a5c227e6678a8e8add2766af24942e31ef002
Robert Ris <take1risk@gmail.com>
2020-01-03 09:00:25 -0600

    dynamically set styles

--- src/App.js
+++ src/App.js
@@ -49,7 +49,8 @@ class App extends Component {
   render() {
 
   	const style = {
-  		backgroundColor: 'white',
+  		backgroundColor: 'green',
+      color: 'white',
   		font: 'inherit',
   		border: '1px solid blue',
   		padding: '8px',
@@ -70,12 +71,22 @@ class App extends Component {
               changed={(event) => this.nameChangedHandler(event, person.id)} />
           })}
         </div>
-        )
+        );
+        style.backgroundColor = 'red';
+    }
+
+    const classes = [];
+    if (this.state.persons.length <= 2) {
+      classes.push('red');
+    }
+    if (this.state.persons.length <= 1) {
+      classes.push('bold');
     }
 
     return (
     <div className="App">
         <h1>Hi, I am a react app</h1>
+        <p className={classes.join(' ')}>It's working</p>
    		<button
    		  style={style}
    		  onClick={this.togglePersonsHandler}>Toggle People
