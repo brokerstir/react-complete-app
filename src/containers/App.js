0567057e1102f1db976d95fae01e68eda8ea66e6
Robert Ris <take1risk@gmail.com>
2020-01-18 08:08:04 -0600

    add component lifecycle

--- src/containers/App.js
+++ src/containers/App.js
@@ -5,6 +5,11 @@ import Cockpit from '../components/Cockpit/Cockpit';
 
 class App extends Component {
 
+  constructor(props) {
+    super(props);
+    console.log('[App.js] constructor');
+  }
+
   state = {
     persons: [
       { id: '1', name: 'Max', age: 28 },
@@ -14,6 +19,24 @@ class App extends Component {
     otherState: 'some other value',
     showPersons: false
   };
+
+  static getDerivedStateFromProps(props, state) {
+    console.log('[App.js] getDerivedStateFromProps', props);
+    return state;
+  }
+
+  componentDidMount() {
+    console.log('[App.js] componentDidMount');
+  }
+
+  shouldComponentUpdate() {
+    console.log('[App.js] shouldComponentUpdate');
+    return true;
+  }
+
+  componentDidUpdate() {
+    console.log('[App.js] componentDidUpdate');
+  }
 
   nameChangedHandler = (event, id) => {
     const personIndex = this.state.persons.findIndex(p => {
@@ -46,8 +69,8 @@ class App extends Component {
 
   render() {
 
+    console.log('[App.js] render');
     let persons = null;
-    let btnClass = null;
 
     if (this.state.showPersons) {
       persons = (
@@ -61,6 +84,7 @@ class App extends Component {
     return (
     <div className={classes.App}>
       <Cockpit
+        title={this.props.appTitle}
         showPersons={this.state.showPersons}
         persons={this.state.persons}
         clicked={this.togglePersonsHandler} />
